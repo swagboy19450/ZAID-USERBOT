c8ad01b53e7f915b71d206d46de8cf608467df38
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-06 14:31:49 +0530

    Update start.py

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
 os.system("git clone https://github.com/Itsunknown-12/ZAID-USERBOT Zaidbot")
-os.chdir("ZAID")
+os.chdir("darkcobra")
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
         stdout=subprocess.PIPE,
