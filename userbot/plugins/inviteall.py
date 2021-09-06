7a80eaf6524f510e1f793b79aceb7a6f73455565
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-06 17:37:26 +0530

    Update inviteall.py

--- userbot/plugins/inviteall.py
+++ userbot/plugins/inviteall.py
@@ -73,7 +73,7 @@ def user_full_name(user):
 
 
 
-@borg.on(admin_cmd(pattern=r"allinvite ?(.*)"))
+@borg.on(admin_cmd(pattern=r"inviteall ?(.*)"))
 async def get_users(event):   
     sender = await event.get_sender() ; me = await event.client.get_me()
     if not sender.id == me.id:
