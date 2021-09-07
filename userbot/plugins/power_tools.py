0cdef369c125edf81fceef0d415ac81c2fd52692
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-07 10:49:34 +0530

    Update power_tools.py

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -22,7 +22,7 @@ async def _(event):
     await asyncio.sleep(1)
     await event.edit("Restarting ▰▰▰▰▰▰▰▰100%...")
     await asyncio.sleep(0.1)
-    await event.edit("Restarted boss....✈ `.king` me or type `.help` or type '.awake' or type '.allive' to check if your DC is online/alive ")
+    await event.edit("Restarted master...⚡ `.ping` me or type `.help` or type '.alive'  to check if your 𝒁𝑨𝑰𝑫 is online/alive ")
     await borg.disconnect()
     os.execl(sys.executable, sys.executable, *sys.argv)
     # You probably don't need it but whatever
